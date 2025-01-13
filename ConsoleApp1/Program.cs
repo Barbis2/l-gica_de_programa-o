852b958ca1bfe0857589d0d0ad015776de9c1308
BARBARA DE OLIVEIRA SILVA <barbara.osilva14@senacsp.edu.br>
2025-01-13 20:05:01 -0300

    ola

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ex03 - Escrever programa que solicite dois numeros inteiros para o usuário e exiba a soma dos número
+
+Console.WriteLine("digite o primeiro número: ");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número:");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(AbandonedMutexException soma
+
+
+Console.WriteLine("Digite o quarto número");
+int numero4 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("A soma é: ") + (numero1 + numero2 + numero3 + numero4));
+
+Console.WriteLine($ " A soma é: {numero1 + numero2 + numero3+ numero4} ");
